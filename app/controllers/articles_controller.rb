76c6f90b4acff2d235b44c4b6fbd889ec597e423
Rails study <souabhgrg713@gmail.com>
2016-04-09 23:23:10 +0530

    dsd

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,6 +8,14 @@ def index
 
 end
 
+def destroy
+
+@article = Article.find(params[:id])
+@article.destroy
+flash[:notice] = "Article was successfully Deleted"
+redirect_to articles_path
+
+end
 
 
 def new
