a48ede67212993f19cd07a208f93746b1c007638
Rails study <souabhgrg713@gmail.com>
2016-04-09 22:48:46 +0530

    ban

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,15 @@
 class ArticlesController < ApplicationController
 
+
+
+def index
+
+@articles = Article.all
+
+end
+
+
+
 def new
 
 @article = Article.new
@@ -18,6 +28,26 @@ else
 end
 end
 
+
+def edit
+
+@article = Article.find(params[:id])	
+
+end
+
+def update
+@article = Article.find(params[:id])
+if @article.update(article_params)
+flash[:notice] = "Article was successfully updated"
+  
+ redirect_to article_path(@article)
+else
+	render 'edit'
+end
+
+end
+
+
 def show
 	@article = Article.find(params[:id])
 
@@ -28,8 +58,5 @@ def article_params
 
 params.require(:article).permit(:title, :description)
 end
-
-
-
 
 end
