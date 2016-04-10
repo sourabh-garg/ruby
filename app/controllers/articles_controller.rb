0312fe1cee43254d3b27c18a2cc98cbac91f2ebf
Rails study <souabhgrg713@gmail.com>
2016-04-10 16:32:07 +0530

    ds

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,7 +12,7 @@ end
 def destroy
 
 @article.destroy
-flash[:notice] = "Article was successfully Deleted"
+flash[:danger] = "Article was successfully Deleted"
 redirect_to articles_path
 
 end
@@ -28,7 +28,7 @@ def create
 @article = Article.new(article_params)
 
 if @article.save
-flash[:notice] = "Article was successfully created"
+flash[:success] = "Article was successfully created"
 redirect_to article_path(@article)
 
 else
@@ -46,7 +46,7 @@ end
 def update
 
 if @article.update(article_params)
-flash[:notice] = "Article was successfully updated"
+flash[:success] = "Article was successfully updated"
   
  redirect_to article_path(@article)
 else
