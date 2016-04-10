8459b2a95415b1ec7cb675f12f84979ad9f22f1e
Rails study <souabhgrg713@gmail.com>
2016-04-11 01:38:03 +0530

    g

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   root 'welcome#home'
   get 'about', to: 'welcome#about'
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
 
   resources :articles
 
