0f3b0462a8207824c1439753aff24d5c2bdd30d5
Rails study <souabhgrg713@gmail.com>
2016-04-12 00:54:48 +0530

    dd

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
 
+before_action :set_user, only: [:edit, :update, :show]
+
 def new
 	@user = User.new
 end
@@ -11,14 +13,12 @@ def index
 end
 
 def edit
-	@user = User.find(params[:id])
+
 
 end
 
 
 def update
-
-@user = User.find(params[:id])
 
 if @user.update(user_params)
 	flash[:success] = "Account updated successfully"
@@ -31,7 +31,7 @@ end
 end
 
 def show
-@user = User.find(params[:id])
+
 @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
 
 end
@@ -50,6 +50,11 @@ end
 end
 
 private
+
+def set_user
+	@user = User.find(params[:id])
+end
+
 def user_params
 	params.require(:user).permit(:username, :email, :password)
 
