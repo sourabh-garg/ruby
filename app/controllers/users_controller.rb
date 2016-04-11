195a9a0cd636bb3c87488fe0057cd15be8b01bad
Rails study <souabhgrg713@gmail.com>
2016-04-11 22:23:12 +0530

    sd

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,11 @@ def new
 	@user = User.new
 end
 
+def index
 
+@users = User.all
+
+end
 
 def edit
 	@user = User.find(params[:id])
@@ -21,10 +25,13 @@ if @user.update(user_params)
 	redirect_to articles_path
 
 else
-
-
+render 'edit'
 
 end
+end
+
+def show
+@user = User.find(params[:id])
 
 end
 
