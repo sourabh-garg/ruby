4a86d4478120ebac21007b8f9cfa69e2c3146175
Rails study <souabhgrg713@gmail.com>
2016-04-12 22:46:58 +0530

    sa

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -27,7 +27,7 @@ end
 
 def create
 @article = Article.new(article_params) 
-@article.user = User.first
+@article.user = current_user
 
 if @article.save
 flash[:success] = "Article was successfully created"
